99feca8cb76f17d0aef34d59b55dcd315ba9f1a2
camanuelsa <149483446+camanuelsa@users.noreply.github.com>
2023-11-28 10:54:08 +0100

    github infame

--- slave_SPI_tb.vhd
+++ slave_SPI_tb.vhd
@@ -7,11 +7,11 @@ END ENTITY;
 ARCHITECTURE behavior OF slave_SPI_tb IS
 
 COMPONENT slave_SPI IS
-PORT(CK, MOSI, nSS, SCK : IN std_logic;
+PORT(CK, MOSI, nSS, SCK, RST_S : IN std_logic;
      MISO : OUT std_logic);
 END COMPONENT;
 
-SIGNAL ck_tb, mosi_tb, nss_tb, sck_tb, miso_tb : std_logic;
+SIGNAL ck_tb, mosi_tb, nss_tb, sck_tb, miso_tb, rst_s_tb : std_logic;
 
 BEGIN
 
@@ -32,10 +32,11 @@ wait for 500 ns;
 END PROCESS;
 
 nss_tb <= '1', '0' after 120 ns,'1' after 35000 ns;
+rst_s_tb <= '0','1' after 34000 ns;
 
 --mosi_tb <= '1','0' after 1700 ns,'1' after 5700 ns, '0' after 6700 ns, '1' after 8700 ns; --lettura 10000100 00100001
 mosi_tb <= '0','1' after 5700 ns, '0' after 6700 ns, '1' after 8700 ns; --scrittura 00000100 00100000
 
-test: slave_SPI PORT MAP(ck_tb, mosi_tb, nss_tb, sck_tb, miso_tb);
+test: slave_SPI PORT MAP(ck_tb, mosi_tb, nss_tb, sck_tb, rst_s_tb, miso_tb);
 
 END behavior;
