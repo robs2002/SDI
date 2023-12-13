8e51767f59b7de3ce6b2210cfddf831a1e71e13c
camanuelsa <149483446+camanuelsa@users.noreply.github.com>
2023-12-13 05:19:26 +0100

    testbench finali spero

--- file_non_in_uso/slave_SPI_tb.vhd
+++ file_non_in_uso/slave_SPI_tb.vhd
@@ -31,11 +31,11 @@ sck_tb <= '1';
 wait for 500 ns;
 END PROCESS;
 
-nss_tb <= '1', '0' after 120 ns,'1' after 35000 ns;
-rst_s_tb <= '1','0' after 50 ns,'1' after 34000 ns;
+nss_tb <= '1', '0' after 120 ns,'1' after 34000 ns;
+rst_s_tb <= '1','0' after 50 ns,'1' after 35000 ns;
 
-mosi_tb <= '0','1' after 2700 ns, '0' after 3700 ns, '1' after 7700 ns; --lettura 00100001
---mosi_tb <= '0','1' after 2700 ns, '0' after 3700 ns, '1' after 8700 ns; --scrittura 00100000
+--mosi_tb <= '0','1' after 2700 ns, '0' after 3700 ns, '1' after 7700 ns; --lettura 00100001
+mosi_tb <= '0','1' after 2700 ns, '0' after 3700 ns, '1' after 8700 ns; --scrittura 00100000
 
 test: slave_SPI PORT MAP(ck_tb, mosi_tb, nss_tb, sck_tb, rst_s_tb, miso_tb);
 
